cb6e4ac25719d2d7c2159c8f03bc73d24e7094b1
Piawai Multi Palaguna <piawaimulti1995@gmail.com>
2014-02-14 23:07:46 +0700

    menambahkan data
    menambah data nim user name dan nama mahasiswa pada project RPL

--- MhsRPLITT.sql
+++ MhsRPLITT.sql
@@ -44,7 +44,7 @@ insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120264','Aulia
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103121285','Taufik Nur Fauzi','taufiknurfauzi');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120259','Muhammad Ikhsan Fahrurrizal','ikhsanfahrurrizal');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120161','Fitriza Marsha Safira','fmarshas');
-
+insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120168','Piawai Multi Palaguna','piawaimulti');
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
