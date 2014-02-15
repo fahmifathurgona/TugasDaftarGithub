8e652efca8d2f0068dbecf6094728f5925afbeee
Puji Muharani <pujmuharani@gmail.com>
2014-02-15 17:31:19 +0700

    Update MhsRPLITT.sql

--- MhsRPLITT.sql
+++ MhsRPLITT.sql
@@ -54,6 +54,7 @@ insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103124327','Paska
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120258','Muhammad Hafidz Al Farabi','mhafidzaf');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120165','Wahyu Maulana','wahyumaulana');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103121286','Muhammad Mushlikh','emush19');
+insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120133','Puji Muharani','pujimuharani');
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
