49df0a227251076ebe5e6780ea6c8169cb74693f
rizkyfadillah <hallidafykzirrr@hotmail.com>
2014-02-15 20:17:04 -0800

    Input Data
    Input data mahasiswa:
    Nama: Rizky Fadillah
    NIM: 1103124330
    Username: rizkyfadillah

--- MhsRPLITT.sql
+++ MhsRPLITT.sql
@@ -56,6 +56,7 @@ insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120165','Wahyu
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103121286','Muhammad Mushlikh','emush19');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120133','Puji Muharani','pujimuharani');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103121271','Tommy Wijaya Sagala','tommyss');
+insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103124330','Rizky Fadillah','rizkyfadillah');
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
