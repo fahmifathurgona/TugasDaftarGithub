db46c45c58d1ce1a76be12fa41dd430afb6fe20f
Fahmi Fathurahman <fahmifathurgona@gmail.com>
2015-03-21 12:00:56 +0700

    Update MhsRPLITT.sql

--- MhsRPLITT.sql
+++ MhsRPLITT.sql
@@ -66,6 +66,7 @@ insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120256','Luthf
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120268','Matahun Dwi Satrio Binar Nuriawan','gilanganeh');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120268','Faiz Satria Syukri','faizsatria');
 insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('1103120269','Putu Gede Bimantara S.P','putugedebimantara');
+insert  into `githubrplitt`(`nim`,`nama`,`username`) values ('10110067','Fahmi Fathurahman','fahmifathurgona');
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
